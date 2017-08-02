ebecf3459638a71ac9cd1733ef7774b12015e0e3
davisilva4222 <davisilva4222@gmail.com>
2017-08-01 21:52:22 -0300

    atualizar exame

--- src/api/controllers/paciente.js
+++ src/api/controllers/paciente.js
@@ -4,6 +4,32 @@ const router = express.Router();
 const pacienteModel = require('../models/paciente');
 const checkAuth = require('../middleware/authMiddleware');
 
+function validForm(payload) {
+    var errors = {};
+    var isValidForm = true;
+
+    if (Object.keys(payload).length === 0 && payload.constructor === Object) {
+        errors["form"] = "O formulário deve ser preenchido!";
+        isValidForm = false;
+    }
+    else {
+        for (atr in payload) {
+            if (payload[atr] == '') {
+                errors[atr] = `O campo ${atr} não pode ser nulo`
+                isValidForm = false;
+            }
+        }
+    }
+
+    return {
+        success: isValidForm,
+        errors
+    }
+
+}
+
+
+
 router.get('/list', checkAuth, function (req, res) {
     pacienteModel.find({}, function (err, result) {
         if (err) {
@@ -18,17 +44,24 @@ router.get('/list', checkAuth, function (req, res) {
     });
 });
 
+/*
+    @params: nome_paciente, nome_exame, cpf, start
+*/
 router.post('/agendarExame', function (req, res) {
-    /*
-        @params: nome_paciente, nome_exame, cpf, start
-    */
+
+    var validationResult = validForm(req.body);
+
+    if (!validationResult.success) {
+        return res.status(400).json({
+            errors: validationResult.errors
+        })
+    }
+
     var p = new pacienteModel();
-    var start_date = req.body.start;
-    console.log(req.body.start);
+    var start_date = moment(req.body.start).format();
     // ADICIONA 30MIN A DATA INICIAL
     var end_date = moment(start_date).add(30, 'm');
-    console.log(end_date);
-
+    end_date = moment(end_date).format();
 
     pacienteModel.findOne({ 'cpf': req.body.cpf }, function (err, paciente) {
         if (err) {
@@ -81,10 +114,37 @@ router.post('/agendarExame', function (req, res) {
 
 });
 
-router.get('/listarExames/', function (req, res) {
-    /*
-        @params: ?nome_exame=String
-    */
+
+/*
+    @params: start, end
+*/
+router.put('/atualizarExame/:id', function (req, res) {
+    pacienteModel.findOneAndUpdate(
+        { "agendamentos._id": req.params.id },
+        {
+            "$set": {
+                "agendamentos.$.start": req.body.start,
+                "agendamentos.$.end": req.body.end
+            }
+        },
+        function (err, result) {
+            if (err) {
+                res.status(500).json({ "error": "Erro ao atualizar agendamento" });
+            }
+            else if (!result) {
+                res.status(400).json({ error: "Nenhum agendamento encontrado" });
+            }
+            else {
+                res.status(200).json({ message: "Agendamento atualizado com sucesso!" });
+            }
+        }
+    );
+});
+
+/*
+    @params: ?nome_exame=String
+*/
+router.get('/listarExames', function (req, res) {
     pacienteModel.aggregate(
         [
             // Match the document containing the array element
@@ -103,6 +163,8 @@ router.get('/listarExames/', function (req, res) {
                     agendamentos: {
                         "$push":
                         {
+                            "exame": "$agendamentos.exame",
+                            "agendamento_id": "$agendamentos._id",
                             "title": "$agendamentos.title",
                             "start": "$agendamentos.start",
                             "end": "$agendamentos.end"
@@ -127,10 +189,20 @@ router.get('/listarExames/', function (req, res) {
 
 
 router.post('/', function (req, res) {
+
+    const validationResult = validForm(req.body);
+
+    if (!validationResult.success) {
+        return res.status(400).json({
+            errors: validationResult.errors
+        })
+    }
+
     var paciente = new pacienteModel();
     paciente.nome = req.body.nome;
     paciente.telefone = req.body.telefone;
     paciente.email = req.body.email;
+    paciente.cpf = req.body.cpf;
 
     paciente.save(function (err) {
         if (err) {
@@ -143,15 +215,31 @@ router.post('/', function (req, res) {
 
 });
 
-router.get('/count', function (req, res) {
-    pacienteModel.count({}, function (err, result) {
+router.get('/summary', function (req, res) {
+    var counter = {};
+    pacienteModel.aggregate([
+        // unwind binding collection
+        { $unwind: "$agendamentos" },
+
+        // group and count by relevant attributes
+        {
+            $group: {
+                _id: 1,
+                count: { $sum: 1 }
+            }
+        }
+    ], function (err, docs) {
         if (err) {
-            res.status(500).json({ err });
+            res.send(err)
         }
         else {
-            res.status(200).json(result);
+            counter["agendamentos"] = docs[0].count;
+            pacienteModel.count({}, function (err, dados) {
+                counter["pacientes"] = dados;
+                res.send(counter);
+            });
         }
-    })
+    });
 });
 
 router.get('/list/:id', function (req, res) {
--- src/api/models/paciente.js
+++ src/api/models/paciente.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 
 const agendamentoSchema = new mongoose.Schema({
     exame: { type: String },
-    start: { type: Date },
-    end: { type: Date },
+    start: { type: String },
+    end: { type: String },
     title: { type: String }
 });
 
 const pacienteSchema = new mongoose.Schema({
     nome: { type: String, required: true },
-    cpf: { type: String },
+    cpf: { type: String, required: true },
     telefone: { type: String },
     email: { type: String },
     data_cad: { type: Date, default: Date.now },
